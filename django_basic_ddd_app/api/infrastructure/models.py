657424ec6af9e5719ccfee0617ea5c3668e39fef
pablo-snz <pablo.snz.brm@gmail.com>
2023-05-26 19:14:43 +0200

    feat: generate models

--- django_basic_ddd_app/api/infrastructure/models.py
+++ django_basic_ddd_app/api/infrastructure/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+import uuid
+
+
+class ProductModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    num_reviews = models.IntegerField()
+    average_rating = models.FloatField(null=True)
+
+
+class ReviewModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_id = models.CharField(max_length=255)
+    product = models.ForeignKey(ProductModel, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    description = models.TextField()
